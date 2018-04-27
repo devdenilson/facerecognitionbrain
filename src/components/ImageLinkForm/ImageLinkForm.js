9bc0c3cf72d4009090c6ea96a1fd2dbcaf3e03e3
Denilson Cabral <Denilson.Cabral@gov.cv>
2018-04-27 21:52:55 -0100

    first commit

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () =>{
+    return(
+        <div>
+            <p className='f3'></p>    
+        </div>
+    );
+}
+
+export default ImageLinkForm;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Tilt from 'react-tilt'
+import Brain from './brain.png'
+import './Logo.css';
+
+const Logo = () =>{
+    return(
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner pa3">
+                <img style={{paddingTop:'5px'}} alt='Brain' src={Brain}/>
+            </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
